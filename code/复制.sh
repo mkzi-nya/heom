93d3ed5bc620426d395128cc311096e10ef89ba9
名字 <>
2025-06-13 18:50:38 +0800

    meow

--- code/复制.sh
+++ code/复制.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+OLD_DIR="$(pwd)"
+cd "$(cd "$(dirname "$0")" && pwd)"
 cp "../heom.txt" "../../IME-Converter-web/dict/ziys-heom.txt"
 cp "../heom.txt" "../安卓/FlyPY_pro/小鹤音形/ziys-heom.txt"
 cp "../heom.txt" "/storage/emulated/0/FlyPY_pro/小鹤音形/ziys-heom.txt"
+cd "$OLD_DIR"
