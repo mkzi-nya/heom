f0cabad955eb80b5489e153cce9b5beed287d056
mkzi_nya <wcnmngsb999@gamil.com>
2025-06-21 22:35:50 +0800

    meow

--- code/复制.sh
+++ code/复制.sh
@@ -2,6 +2,6 @@
 OLD_DIR="$(pwd)"
 cd "$(cd "$(dirname "$0")" && pwd)"
 cp "../ziys/heom.txt" "../../IME-Converter-web/dict/ziys-heom.txt"
-cp "../ziys/heom.txt" "../安卓/FlyPY_pro/小鹤音形/ziys-heom.txt"
+cp "../ziys/heom.txt" "../android/FlyPY_pro/小鹤音形/ziys-heom.txt"
 cp "../ziys/heom.txt" "/storage/emulated/0/FlyPY_pro/小鹤音形/ziys-heom.txt"
 cd "$OLD_DIR"
