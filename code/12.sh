139044e52473dab057d1e1dcafad11395f31c968
名字 <>
2025-07-05 14:33:09 +0800

    meow

--- code/12.sh
+++ code/12.sh
@@ -7,6 +7,5 @@ cp "../heom.txt" "../../IME-Converter-web/dict/heom.txt"
 cp "../ziys/heom.schema.yaml" "/Users/mingzi/Library/Rime"
 cp "../ziys/heom.dict.yaml" "/Users/mingzi/Library/Rime"
 cp "../heom.txt" "../android/鹤仓/"
-sh 转格式.sh
 sh push.sh
 cd "$OLD_DIR"
