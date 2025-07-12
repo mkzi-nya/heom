9598aa68c411b797eee5093997e90203ed9d22b7
mkzi_nya <wcnmngsb999@gamil.com>
2025-07-12 20:06:51 +0800

    meow

--- code/11.sh
+++ code/11.sh
@@ -7,5 +7,6 @@ cp "../heom.txt" "../../IME-Converter-web/dict/heom.txt"
 cp "../heom.txt" "/storage/emulated/0/Android/data/nya.IME/files/ime_data_dir/鹤仓/heom.txt"
 cp "../ziys/用户.txt" "/storage/emulated/0/Android/data/nya.IME/files/ime_data_dir/鹤仓/用户.txt"
 cp "../heom.txt" "../android/鹤仓/"
+find ../*/ -type f -name "*.bak" -delete
 sh push.sh
 cd "$OLD_DIR"
