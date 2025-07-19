5460d819f961064de3db41f8cce96a6dd40f7366
mkzi_nya <wcnmngsb999@gamil.com>
2025-07-19 19:30:57 +0800

    meow

--- code/11.sh
+++ code/11.sh
@@ -2,7 +2,7 @@
 OLD_DIR="$(pwd)"
 cd "$(cd "$(dirname "$0")" && pwd)"
 sh 拼接.sh
-cp "../ziys/heom.txt" "../../IME-Converter-web/dict/ziys-heom.txt"
+cp "../ziys/heom_s.txt" "../../IME-Converter-web/dict/ziys-heom.txt"
 cp "../heom.txt" "../../IME-Converter-web/dict/heom.txt"
 cp "../heom.txt" "/storage/emulated/0/Android/data/nya.IME/files/ime_data_dir/鹤仓/heom.txt"
 cp "../ziys/用户.txt" "/storage/emulated/0/Android/data/nya.IME/files/ime_data_dir/鹤仓/用户.txt"
