5d6a583bc24f30862d6ee182a20696b753ddd7e3
名字 <>
2026-01-17 23:01:31 +0800

    meow

--- code/push.sh
+++ code/push.sh
@@ -1,6 +1,4 @@
 OLD_DIR="$(pwd)"
 cd ..
 git add . && git commit -m "meow" && git push origin main
-cd ../IME-Converter-web/
-git add . && git commit -m "meow" && git push origin main
 cd "$OLD_DIR"
