07752d1efc28b541662f7692135895cd53496636
名字 <>
2026-06-26 20:56:19 +0800

    meow

--- code/拼接.sh
+++ code/拼接.sh
@@ -27,16 +27,16 @@ for file in ../heom.txt 3.txt; do
         echo "警告：文件 $file 不存在，跳过。"
     fi
 done
-for file in 3.txt 4.txt; do
+for file in 0.txt ../heom.txt; do
     if [ -f "$file" ]; then
-        cat "$file" >> "../heom.txt"
+        cat "$file" >> "../heom.dict.yaml"
     else
         echo "警告：文件 $file 不存在，跳过。"
     fi
 done
-for file in 0.txt ../heom.txt; do
+for file in 3.txt 4.txt; do
     if [ -f "$file" ]; then
-        cat "$file" >> "../heom.dict.yaml"
+        cat "$file" >> "../heom.txt"
     else
         echo "警告：文件 $file 不存在，跳过。"
     fi
